5f316a144f65556fe3ae2f174fc6cd2e13239e64
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-05 21:35:30 -0700

    Fixed hour hand and converted from military time

--- Day2/index.js
+++ Day2/index.js
@@ -3,17 +3,29 @@ window.onload = function() {
   let minuteHand = document.querySelector(".min-hand");
   let hourHand = document.querySelector(".hour-hand");
 
-  function getDegrees(time) {
+  function getDegrees(time, unit) {
+    if(unit === "hour") {
+      return ((time * 360) / 12) + 90;
+    }
     return ((time * 360) / 60) + 90;
+  }
+  function convertMilitaryTime(hour) {
+    if(hour === 0) {
+      return (hour + 1);
+    }
+    if (hour > 12) {
+      return (hour - 12);
+    }
   }
   function getTime() {
     let start = new Date();
     let seconds = start.getSeconds();
     let minutes = start.getMinutes();
-    let hours = start.getHours();
+    let hours = convertMilitaryTime(start.getHours());
+    console.log(hours);
     secondHand.style.transform = "rotate(" + getDegrees(seconds) + "deg)";
     minuteHand.style.transform = "rotate(" + getDegrees(minutes) + "deg)";
-    hourHand.style.transform = "rotate(" + getDegrees(hours) + "deg)";
+    hourHand.style.transform = "rotate(" + getDegrees(hours, "hour") + "deg)";
   }
 
   setInterval(getTime, 1000);
