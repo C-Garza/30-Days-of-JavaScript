a766d6e4d618b4d11f07ab916259f52bbc2688dd
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-05 21:01:25 -0700

    Add getTime and getDegrees function

--- Day2/index.js
+++ Day2/index.js
@@ -1,3 +1,20 @@
 window.onload = function() {
-  
+  let secondHand = document.querySelector(".second-hand");
+  let minuteHand = document.querySelector(".min-hand");
+  let hourHand = document.querySelector(".hour-hand");
+
+  function getDegrees(time) {
+    return ((time * 360) / 60) + 90;
+  }
+  function getTime() {
+    let start = new Date();
+    let seconds = start.getSeconds();
+    let minutes = start.getMinutes();
+    let hours = start.getHours();
+    secondHand.style.transform = "rotate(" + getDegrees(seconds) + "deg)";
+    minuteHand.style.transform = "rotate(" + getDegrees(minutes) + "deg)";
+    hourHand.style.transform = "rotate(" + getDegrees(hours) + "deg)";
+  }
+
+  setInterval(getTime, 1000);
 };
