8da99f70fec6d0f4afe89a6982fe11605a9f3005
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-11 01:42:01 -0700

    Add handleUpdate function and event listener for inputs

--- Day3/index.js
+++ Day3/index.js
@@ -1,3 +1,11 @@
 window.onload = function() {
-  
+  let inputs = document.querySelectorAll(".controls input");
+  function handleUpdate() {
+    let hasPx = false;
+    if(this.dataset.sizing) {
+      hasPx = true;
+    }
+    document.documentElement.style.setProperty("--" + this.name, this.value + (hasPx ? "px" : ""));
+  }
+  inputs.forEach(input => input.addEventListener("input", handleUpdate));
 };
