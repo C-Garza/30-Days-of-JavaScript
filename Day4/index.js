55e53a16f78786aedd21b02d31966ccccca4ce97
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-12 01:44:20 -0700

    Add filter and map to inventors.

--- Day4/index.js
+++ Day4/index.js
@@ -17,10 +17,17 @@ window.onload = function() {
   const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
   // Array.prototype.filter()
   // 1. Filter the list of inventors for those who were born in the 1500's
+  let bornIn1500s = inventors.filter(person => {
+    return person.year <= 1599 && person.year >= 1500;
+  });
+  console.table(bornIn1500s);
 
   // Array.prototype.map()
   // 2. Give us an array of the inventors' first and last names
-
+  let names = inventors.map(person => {
+    return person.first + " " + person.last;
+  });
+  console.table(names);
   // Array.prototype.sort()
   // 3. Sort the inventors by birthdate, oldest to youngest
 
