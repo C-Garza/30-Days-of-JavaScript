8b6f178a20ecd4996b046f97f7ecb7db5cdd1c4e
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-15 02:12:32 -0700

    Add fetch and displayMatches and findMatches function.

--- Day6/index.js
+++ Day6/index.js
@@ -1,3 +1,25 @@
 window.onload = function() {
   const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+  let cities = [];
+  let searchInput = document.querySelector(".search");
+  let suggestions = document.querySelector(".suggestions");
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+      data.map((city) => {
+        cities.push(city);
+      });
+    })
+    .catch(error => console.log("Error is ", error));
+    function findMatches(word, cities) {
+      return cities.filter(place => {
+        let regex = new RegExp(word, "gi");
+        return place.city.match(regex) || place.state.match(regex);
+      });
+    }
+    function displayMatches() {
+      let matchArr = findMatches(this.value, cities);
+      console.log(matchArr);
+    }
+    searchInput.addEventListener("input", displayMatches);
 };
