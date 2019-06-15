82f84f117d671311c3a3fd83b56a05af62a650ea
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-14 18:29:20 -0700

    Add event listeners and toggle functions

--- Day5/index.js
+++ Day5/index.js
@@ -1,3 +1,13 @@
 window.onload = function() {
-  
+  let panels = document.querySelector(".panels");
+  function toggleOpen(e) {
+    e.target.closest(".panel").classList.toggle("open");
+  }
+  function toggleActive(e) {
+    if(e.propertyName.includes("flex")) {
+      e.target.closest(".panel").classList.toggle("open-active");
+    }
+  }
+  panels.addEventListener("click", toggleOpen);
+  panels.addEventListener("transitionend", toggleActive);
 };
