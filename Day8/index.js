ae9e8f1a22e1c7b2f23529c725b5a58261632435
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-17 15:24:40 -0700

    Initialize canvas settings.

--- Day8/index.js
+++ Day8/index.js
@@ -1,3 +1,9 @@
 window.onload = function() {
-  
+  let canvas = document.querySelector("#draw");
+  let ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.strokeStyle = "#BADA55";
+  ctx.lineJoin = "round";
+  ctx.lineCap = "round";
 };
