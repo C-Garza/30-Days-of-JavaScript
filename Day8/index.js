83fb1bc9ef9110ddfb7f9c96969769e096a0bee0
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-17 15:32:58 -0700

    Add event listeners on canvas.

--- Day8/index.js
+++ Day8/index.js
@@ -6,4 +6,18 @@ window.onload = function() {
   ctx.strokeStyle = "#BADA55";
   ctx.lineJoin = "round";
   ctx.lineCap = "round";
+
+  let isDrawing = false;
+  let lastX = 0;
+  let lastY = 0;
+
+  function draw(e) {
+    if(!isDrawing) return;
+    console.log(e);
+  }
+
+  canvas.addEventListener("mousemove", draw);
+  canvas.addEventListener("mousedown", () => isDrawing = true);
+  canvas.addEventListener("mouseup", () => isDrawing = false);
+  canvas.addEventListener("mouseout", () => isDrawing = false);
 };
