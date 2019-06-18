b245b5ae4222632d8d151c67cdf1117d0c910d52
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-17 23:21:55 -0700

    Add dir, group, count, time, and table console commands.

--- Day9/index.js
+++ Day9/index.js
@@ -37,11 +37,38 @@ window.onload = function() {
   console.clear();
 
   // Viewing DOM Elements
+  console.log(p);
+  console.dir(p);
 
   // Grouping together
+  dogs.forEach(dog => {
+    console.groupCollapsed(dog.name);
+    console.log("This is " + dog.name + ".");
+    console.log(dog.name + " is " + dog.age + " years old.");
+    console.log(dog.name + " is " + (dog.age * 7) + " dog years old.");
+    console.groupEnd(dog.name);
+  });
 
   // counting
+  console.count("Chris");
+  console.count("Wes");
+  console.count("Chris");
+  console.count("Chris");
+  console.count("Chris");
+  console.count("Wes");
+  console.count("Chris");
+  console.count("Wes");
+  console.count("Wes");
 
   // timing
+  console.time("fetching data");
+  fetch("https://api.github.com/users/c-garza")
+    .then(response => response.json())
+    .then(data => {
+      console.timeEnd("fetching data");
+      console.log(data);
+    });
+
+    console.table(dogs);
 
 };
