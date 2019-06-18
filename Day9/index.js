116b7ecebeaf1c5667b37ba59d6cc389435ac748
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-17 23:00:55 -0700

    Add regular and interpolated console.log.

--- Day9/index.js
+++ Day9/index.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+  let stringVar = "new";
 
   function makeGreen() {
     const p = document.querySelector('p');
@@ -9,8 +10,11 @@ window.onload = function() {
   document.querySelector("p").onclick = makeGreen;
 
   // Regular
+  console.log("hello");
 
   // Interpolated
+  console.log("Hello I am a %s string!", "new");
+  console.log(`Hello I am a ${stringVar} string!`);
 
   // Styled
 
