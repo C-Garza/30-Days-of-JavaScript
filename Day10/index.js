41b28fc5ee4e29e802cd4f0bba3bb5f8aefec96b
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-18 17:21:31 -0700

    Change event listener to click with shiftKey property.

--- Day10/index.js
+++ Day10/index.js
@@ -1,13 +1,14 @@
 window.onload = function() {
   let checkBoxes = document.querySelectorAll(".item input");
+  let inbox = document.querySelector(".inbox");
   
   function shiftSelect() {
     
   }
 
-  window.addEventListener("keydown", (e) => {
-    console.log(e.keyCode); //SHIFT KEYCODE 16
-    if(e.keyCode === 16) {
+  window.addEventListener("click", (e) => {
+    console.log(e); //HAS shiftKey PROPERTY
+    if(e.shiftKey) {
       shiftSelect();
     }
   });
