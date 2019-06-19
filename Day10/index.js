89619b3ea2a6e5d60577a45ad719ce52c4e44320
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-18 19:59:33 -0700

    Add shiftSelect function.

--- Day10/index.js
+++ Day10/index.js
@@ -3,18 +3,30 @@ window.onload = function() {
   let checkedBoxes = document.querySelectorAll(".item input:checked");
   let inbox = document.querySelector(".inbox");
   
-  function shiftSelect() {
-    
+  function shiftSelect(e) {
+    let firstCheck = false;
+    let lastCheckedIndex = Array.from(checkBoxes).indexOf(checkedBoxes[checkedBoxes.length - 1]);
+    for(let i = 0; i < checkBoxes.length; i++) {
+      if(checkBoxes[i].checked === true && !firstCheck) {
+        firstCheck = true;
+      }
+      if(firstCheck) {
+        checkBoxes[i].checked = true;
+      }
+      if(lastCheckedIndex === i) {
+        return;
+      }
+    }
   }
 
   inbox.addEventListener("click", (e) => {
     if(event.target.tagName === "LABEL") {
       return;
     }
-    checkedBoxes = document.querySelectorAll(".item input:checked");
+    checkedBoxes = Array.from(document.querySelectorAll(".item input:checked"));
     console.log(e.target); //HAS shiftKey PROPERTY
-    if(e.shiftKey && (checkedBoxes.length > 1)) {
-      shiftSelect();
+    if(e.shiftKey && (checkedBoxes.length > 1) && e.target.checked) {
+      shiftSelect(e);
     }
   });
 };
