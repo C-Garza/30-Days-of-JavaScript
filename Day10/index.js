eb023355b1a1bb4ec2541f6c74c13007a69fdc5c
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-18 17:03:02 -0700

    Add event listener and shiftSelect function.

--- Day10/index.js
+++ Day10/index.js
@@ -1,3 +1,14 @@
 window.onload = function() {
+  let checkBoxes = document.querySelectorAll(".item input");
   
+  function shiftSelect() {
+    
+  }
+
+  window.addEventListener("keydown", (e) => {
+    console.log(e.keyCode); //SHIFT KEYCODE 16
+    if(e.keyCode === 16) {
+      shiftSelect();
+    }
+  });
 };
