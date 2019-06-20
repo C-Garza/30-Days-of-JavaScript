42899e95142156aceca08e56e894d0c3694113e6
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-20 00:24:22 -0700

    Add selector variables and togglePlay function.

--- Day11/index.js
+++ Day11/index.js
@@ -1,3 +1,18 @@
 window.onload = function() {
-  
+  let player = document.querySelector(".player");
+  let video = player.querySelector(".viewer");
+  let progress = player.querySelector(".progress");
+  let progressBar = player.querySelector(".progress__filled");
+  let toggle = player.querySelector(".toggle");
+  let ranges = player.querySelectorAll(".player__slider");
+  let skipButtons = player.querySelectorAll("[data-skip]");
+
+  function togglePlay() {
+    if(video.paused) {
+      video.play();
+    }
+    else {
+      video.pause();
+    }
+  }
 };
