c566fd21982efe331b8305aaafee8d29a1a79a38
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-06-22 16:36:27 -0700

    Add key event listener and detect KONAMI code sequence.

--- Day12/index.js
+++ Day12/index.js
@@ -1,3 +1,14 @@
 window.onload = function() {
+  let pressed = [];
+  let sequence = "38384040373937396665";
   
+  window.addEventListener("keyup", (e) => {
+    pressed.push(e.keyCode);
+    pressed.splice(-sequence.length / 2 - 1, pressed.length - sequence.length / 2);
+    if(pressed.join("").includes(sequence)) {
+      console.log("ALL YOUR BASE ARE BELONG TO US");
+      cornify_add();
+    }
+    console.log(pressed);
+  });
 };
